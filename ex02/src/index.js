112f4a10c72bffaeebff43ee5f0490377561baa5
Haris Pasic <harispasic@ymail.com>
2022-03-31 12:55:30 +0200

    Increment myRes

---
+++ ex02/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+  var myRes = 75;
+  //Only change code belowe this line
+  myRes++;
+  //Only change code above this line
+  return myRes;
+}
+console.log(main());
+module.exports = main;
