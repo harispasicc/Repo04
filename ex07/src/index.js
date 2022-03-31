065b1c4e73fcffdeee7b9ab1d33ffe19480b6bb9
Haris Pasic <harispasic@ymail.com>
2022-03-31 14:22:04 +0200

    Finding a remainder

---
+++ ex07/src/index.js
@@ -0,0 +1,15 @@
+function main(numOne, numTwo) {
+  //Only change code belowe this line
+  int = numOne;
+  int = numTwo;
+  var reminder;
+
+  reminder = numOne % numTwo;
+  //Only change code above this line
+  return reminder;
+}
+console.log(main(5, 2)); //change this line
+console.log(main(15, 4)); //change this line
+console.log(main(102, 25)); //change this line
+
+module.exports = main;
