582faf9810609fb377dd16f1b5b19c567199cc07
Haris Pasic <harispasic@ymail.com>
2022-03-31 14:18:29 +0200

    Decimal Numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+  //Only change code belowe this line
+  var myDecimal = (9).toFixed(2);
+  var myDecimalE = 2.54e-3;
+
+  //Only change code above this line
+  return {
+    myDecimal,
+    myDecimalE,
+  };
+}
+console.log(main());
+module.exports = main;
